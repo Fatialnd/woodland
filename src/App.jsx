b91f265ec5b65715120b811a6e9e4c9fc0cde11f
Fatemeh Alinejad <fatemeh.alinejad.dev@gmail.com>
2025-02-10 21:16:39 +0100

    modified Button component and created Row component

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: orange;
+  padding: 20px;
 `;
 
 function App() {
@@ -13,11 +14,28 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading type="h1">WoodLand</Heading>
-        <Heading as="h2">WoodLand</Heading>
-        <Heading as="h3">WoodLand</Heading>
-        <Button>Click Here</Button>
-        <Input type="number" placeholder="guests number" />
+        <Row type="vertical">
+          <Row type="horizontal">
+            <Heading type="h1">WoodLand</Heading>
+
+            <div>
+              <Heading as="h2">Check in and Check out</Heading>
+              <Button variation="primary" size="medium">
+                Check in
+              </Button>
+              <Button variation="secondary" size="small">
+                Check out
+              </Button>
+            </div>
+          </Row>
+          <Row type="vertical">
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
