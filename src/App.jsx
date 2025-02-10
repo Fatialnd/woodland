fb0ce79564ea925aff66411e34c3608f1b1d027e
Fatemeh Alinejad <fatemeh.alinejad.dev@gmail.com>
2025-02-10 17:02:37 +0100

    reusable styled components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
 `;
 
+const StyledApp = styled.div`
+  background-color: orange;
+`;
+
 function App() {
   return (
-    <div>
-      <div>
+    <>
+      <GlobalStyles />
+      <StyledApp>
         <H1>WoodLand</H1>
-      </div>
-    </div>
+        <Button>Click Here</Button>
+        <Input type="number" placeholder="guests number" />
+      </StyledApp>
+    </>
   );
 }
 
