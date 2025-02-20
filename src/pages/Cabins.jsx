7b1f6ccd8f31df5d021605aebac58d484cc79cd3
Fatemeh Alinejad <fatemeh.alinejad.dev@gmail.com>
2025-02-20 13:02:10 +0100

    refactoring

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,24 +1,28 @@
+import { useState } from "react";
+import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import CabinTable from "../features/cabins/CabinTable";
-import { useState } from "react";
 import Button from "../ui/Button";
 import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
   const [showForm, setShowForm] = useState(false);
+
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Filter / sort</p>
+        <p>Filter / Sort</p>
       </Row>
-      <CabinTable />
 
-      <Button onClick={() => setShowForm((show) => !show)} variation="primary">
-        Add new cabin
-      </Button>
-      {showForm && <CreateCabinForm />}
+      <Row>
+        <CabinTable />
+
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
+      </Row>
     </>
   );
 }
