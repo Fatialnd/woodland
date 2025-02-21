eff676c2792fe59be426170987dad684fc9efae3
Fatemeh Alinejad <fatemeh.alinejad.dev@gmail.com>
2025-02-21 14:31:37 +0100

    adding typescript

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Toaster } from "react-hot-toast";
+import { Toaster, ToastOptions, ToastType } from "react-hot-toast";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,7 +21,60 @@ const queryClient = new QueryClient({
   },
 });
 
-function App() {
+const toastOptions: Record<ToastType, ToastOptions> = {
+  success: {
+    duration: 3000,
+    style: {
+      fontSize: "16px",
+      maxWidth: "500px",
+      padding: "16px 24px",
+      backgroundColor: "var(--color-grey-0)",
+      color: "var(--color-grey-700)",
+    },
+  },
+  error: {
+    duration: 5000,
+    style: {
+      fontSize: "16px",
+      maxWidth: "500px",
+      padding: "16px 24px",
+      backgroundColor: "var(--color-grey-0)",
+      color: "var(--color-grey-700)",
+    },
+  },
+  loading: {
+    duration: 4000,
+    style: {
+      fontSize: "16px",
+      maxWidth: "500px",
+      padding: "16px 24px",
+      backgroundColor: "var(--color-grey-0)",
+      color: "var(--color-grey-700)",
+    },
+  },
+  blank: {
+    duration: 4000,
+    style: {
+      fontSize: "16px",
+      maxWidth: "500px",
+      padding: "16px 24px",
+      backgroundColor: "var(--color-grey-0)",
+      color: "var(--color-grey-700)",
+    },
+  },
+  custom: {
+    duration: 4000,
+    style: {
+      fontSize: "16px",
+      maxWidth: "500px",
+      padding: "16px 24px",
+      backgroundColor: "var(--color-grey-0)",
+      color: "var(--color-grey-700)",
+    },
+  },
+};
+
+const App: React.FC = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
@@ -46,27 +99,13 @@ function App() {
       </BrowserRouter>
 
       <Toaster
-        position="top-center"
-        gutter={12}
-        containerStyle={{ margin: "8px" }}
-        toastOptions={{
-          success: {
-            duration: 3000,
-          },
-          error: {
-            duration: 5000,
-          },
-          style: {
-            fontSize: "16px",
-            maxWidth: "500px",
-            padding: "16px 24px",
-            backgroundColor: "var(--color-grey-0)",
-            color: "var(--color-grey-700)",
-          },
-        }}
+        position="top-center" // Valid ToastPosition
+        containerStyle={{ margin: "8px" }} // Valid CSS property
+        gutter={12} // Valid Toaster property
+        toastOptions={toastOptions} // Pass toastOptions here
       />
     </QueryClientProvider>
   );
-}
+};
 
 export default App;
