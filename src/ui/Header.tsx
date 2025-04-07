259e2c45630526886d262284c25f96d8f710a701
Fatemeh Alinejad <fatemeh.alinejad.dev@gmail.com>
2025-04-07 23:09:59 +0200

    logout

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,12 +1,19 @@
 import styled from "styled-components";
+import Logout from "../features/authentication/Logout";
 
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
   padding: 3.2rem 2.4rem;
 `;
 
-function Header() {
-  return <StyledHeader>Header</StyledHeader>;
+function Header(): JSX.Element {
+  return (
+    <>
+      <StyledHeader>
+        <Logout />
+      </StyledHeader>
+    </>
+  );
 }
 
 export default Header;
