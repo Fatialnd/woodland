c8f0ef0b21c62987188e7a03e9379cb10f215d21
Fatemeh Alinejad <Fati@Mac.home>
2025-05-28 14:53:06 +0200

    remove credentials

--- src/features/authentication/Logout.tsx
+++ src/features/authentication/Logout.tsx
@@ -1,13 +1,13 @@
-import { HiArrowRightOnRectangle } from "react-icons/hi2";
-import ButtonIcon from "../../ui/ButtonIcon";
-import { useLogout } from "./useLogout";
-import SpinnerMini from "../../ui/SpinnerMini";
+import { HiArrowRightOnRectangle } from 'react-icons/hi2';
+import ButtonIcon from '../../ui/ButtonIcon';
+import { useLogout } from './useLogout';
+import SpinnerMini from '../../ui/SpinnerMini';
 
 function Logout() {
   const { logout, isLoading } = useLogout();
   return (
     <ButtonIcon disabled={isLoading} onClick={() => logout()}>
-    {!isLoading ?  <HiArrowRightOnRectangle /> : <SpinnerMini />}
+      {!isLoading ? <HiArrowRightOnRectangle /> : <SpinnerMini />}
     </ButtonIcon>
   );
 }
